0de7e70690fb68357aeca00c2886447cb0b99480
DaliaMatus <daliamatus@gmail.com>
2020-07-16 20:01:07 -0500

    added sign in functionality

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,10 +2,12 @@ import {
     SET_USER_PURCHASES,
     SET_PURCHASE_DETAIL,
     SET_CART_PRODUCTS,
-    ADD_CART_PRODUCT
+    ADD_CART_PRODUCT,
+    AUTHENTICATE_USER
 } from '../actions/types';
 
 const INITIAL_STATE = {
+    user: {},
     cartProducts: [],
     purchases: [],
     purchaseDetail: {
@@ -23,28 +25,34 @@ const INITIAL_STATE = {
 
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
-    case ADD_CART_PRODUCT:
-            var exists = false
-            const newCP = action.payload;
-            var cartProducts = []
-            state.cartProducts.map(cartProduct => {
-                if(cartProduct.product._id == newCP._id) {
-                    exists = true
-                    cartProduct.quantity += 1;
+        case AUTHENTICATE_USER: 
+                const { user } = action.payload;
+                return {
+                    ...state,
+                    user
                 }
-                cartProducts.push(cartProduct);
-            })
-            if(exists == false)  {
-                cartProducts.push({
-                    _id: state.cartProducts.length + 1,
-                    product: newCP,
-                    quantity: 1
+        case ADD_CART_PRODUCT:
+                var exists = false
+                const newCP = action.payload;
+                var cartProducts = []
+                state.cartProducts.map(cartProduct => {
+                    if(cartProduct.product._id == newCP._id) {
+                        exists = true
+                        cartProduct.quantity += 1;
+                    }
+                    cartProducts.push(cartProduct);
                 })
-            }
-            return {
-                ...state,
-                cartProducts: cartProducts
-            }
+                if(exists == false)  {
+                    cartProducts.push({
+                        _id: state.cartProducts.length + 1,
+                        product: newCP,
+                        quantity: 1
+                    })
+                }
+                return {
+                    ...state,
+                    cartProducts: cartProducts
+                }
         case SET_CART_PRODUCTS:
             return {
                 ...state,
